348173a9a48900094ca9aa60277a1b20764bcaa0
Francisc Munteanu <munteanu.francisc@gmail.com>
2015-12-17 22:16:39 +0100

    Diagonal Difference

---
+++ DiagonalDifference.cpp
@@ -0,0 +1,57 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector< vector<int> > a(n,vector<int>(n));
+     int sumL = 0;
+    int sumR = 0;
+    for(int a_i = 0;a_i < n;a_i++){
+       for(int a_j = 0;a_j < n;a_j++){
+          cin >> a[a_i][a_j];
+           if(a_i == a_j)
+               {
+             sumL += a[a_i][a_j];  
+           }
+           if(a_i + a_j == n - 1){
+               sumR += a[a_i][a_j];
+           }
+       }
+    }
+    
+    int diff= 0;
+    if(sumR >= sumL){
+    diff = sumR - sumL;
+    }else{
+     diff = sumL - sumR;       
+    }
+
+    cout << diff;
+    return 0;
+}
+
+
