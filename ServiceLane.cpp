fbd490a54cdde81f6d94884a4ee49ab76a3dae72
Francisc Munteanu <munteanu.francisc@gmail.com>
2015-12-22 21:32:36 +0100

    Service Lane

---
+++ ServiceLane.cpp
@@ -0,0 +1,42 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+  int n;
+  int t;
+  cin >> n >> t;
+  vector<int> width(n);
+  for(int width_i = 0;width_i < n;width_i++){
+    cin >> width[width_i];
+  }
+  for(int a0 = 0; a0 < t; a0++){
+    int i;
+    int j;
+    cin >> i >> j;
+  }
+  return 0;
+}
+
