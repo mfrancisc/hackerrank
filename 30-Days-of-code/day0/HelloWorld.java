8c2d7fda47a34d42adc4eafcae32a6f11cfd5677
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-01-01 13:19:25 +0100

    30 Days of code - day 0

---
+++ 30-Days-of-code/day0/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorld {
+
+  public static void main(String args[])
+  {
+    System.out.println("Hello World."); 
+    System.out.println("Welcome to 30 Days of Code."); 
+  }
+}
