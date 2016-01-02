1a5d75ec533bb3d06434d5dc04b0c7f396de6abe
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-01-02 15:13:21 +0100

    Artihmetic - day 2

---
+++ 30-Days-of-code/day2/Arithmetic.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Arithmetic {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    double M = sc.nextDouble(); // original meal price
+    int T = sc.nextInt(); // tip percentage
+    int X = sc.nextInt(); // tax percentage
+
+    double tip = (T * M)/ 100;
+    double tax = (X * M) / 100;
+    double price = M + tip + tax; 
+
+    int total = (int) Math.round(price);
+
+    System.out.println("The final price of the meal is $" + total + ".");
+  }
+}
+
