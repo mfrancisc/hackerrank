157213a309722afce87ef73db104c4e779640051
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-01-04 22:10:44 +0100

    Day 4: Logical Operators + Class vs. Instance

---
+++ 30-Days-of-code/day4/Person.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Person {
+
+  int age;
+  public Person(int initial_Age) {
+    if(initial_Age < 0)
+    {
+      System.out.println("This person is not valid, setting age to 0.");
+      initial_Age = 0;
+    }
+
+    age = initial_Age;
+  }
+
+  public void amIOld() {
+    if(age < 13){
+      System.out.println("You are young.");
+    }else if (age >= 13 && age < 18)
+    {
+      System.out.println("You are a teenager.");
+
+    }else if(age >= 18)
+    {
+      System.out.println("You are old."); 
+    }
+  }
+
+  public void yearPasses() {
+    age++;
+  }
+
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    for (int i = 0; i < T; i++) {
+      int age = sc.nextInt();
+      Person p = new Person(age);
+      p.amIOld();
+      for (int j = 0; j < 3; j++) {
+        p.yearPasses();
+      }
+      p.amIOld();
+      System.out.println();
+    }
+  }
+}
+
