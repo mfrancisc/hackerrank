890f3d91becb39c6283b405f5bdf6118d227415b
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-01-09 21:32:33 +0100

    day-9-recursion

---
+++ 30-Days-of-code/day9/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int gcd=find_gcd(a,b);
+    System.out.println(gcd);
+  }
+  static  int find_gcd(int a,int b){
+    if(a == b)
+    {
+      return a;
+    }else{
+      if(a > b){
+        return find_gcd(b,a - b);
+      }else{
+        return find_gcd(b -a, a); 
+      }
+    }
+  }
+}
+
+
