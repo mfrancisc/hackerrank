8ae6b1ed367e47600768bd79a011bc9cdeccf585
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-01-18 10:04:42 +0100

    day-17-exceptions

---
+++ 30-Days-of-code/day17/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+
+  public static void main(String []argh)
+  {
+    Scanner in = new Scanner(System.in);
+    int T=in.nextInt();
+    while(T-->0)
+    {
+      int n = in.nextInt();
+      int p = in.nextInt();
+      Calculator myCalculator = new Calculator();
+      try
+      {
+        int ans=myCalculator.power(n,p);
+        System.out.println(ans);
+
+      }
+      catch(Exception e)
+      {
+        System.out.println(e.getMessage());
+      }
+    }
+
+  }
+}
+
