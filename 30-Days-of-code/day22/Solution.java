f1295aa5ef4974944875225231577edcdff8a646
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-01-22 19:36:40 +0100

    day-22-binary-search-trees

---
+++ 30-Days-of-code/day22/Solution.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+
+  public static int getHeight(Node root){
+    if(root == null)
+    {
+      return 0;
+    } else {
+      return 1 + Math.max(getHeight(root.left),
+            getHeight(root.right));
+    }
+  }
+  public static Node insert(Node root,int data){
+    if(root==null){
+      return new Node(data);
+    }
+    else{
+      Node cur=new Node(data);
+      if(data<=root.data){
+        cur=insert(root.left,data);
+        root.left=cur;
+      }
+      else{
+        cur=insert(root.right,data);
+        root.right=cur;
+      }
+      return root;
+    }
+  }
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int T=sc.nextInt();
+    Node root=null;
+    while(T-->0){
+      int data=sc.nextInt();
+      root=insert(root,data);
+    }
+    int height=getHeight(root);
+    System.out.println(height);
+  }
+}
+
