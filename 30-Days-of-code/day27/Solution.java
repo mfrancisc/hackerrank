7bdd1c6afaf2f201b73dd3e24393ec7d18db2697
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-01-30 02:21:40 +0100

    day-27-testing-part-2

---
+++ 30-Days-of-code/day27/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  public static void main(String[] args) 
+  {
+    System.out.println("5");
+    System.out.println("5 4");
+    System.out.println("-1 1 0 -5 1");
+    System.out.println("6 3");
+    System.out.println("-1 0 1 -15 1 2");
+    System.out.println("8 5");
+    System.out.println("-1 2 0 -15 1 -3 1 2");
+    System.out.println("4 2");
+    System.out.println("-1 -2 0 2");
+    System.out.println("9 7");
+    System.out.println("-1 0 2 2 3 4 5 6 7");
+  }
+
+}
+
